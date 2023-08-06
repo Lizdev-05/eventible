1519087167157070f76abf6dcda721a67fd1cad1
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-06 05:47:41 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import AddNewMeetUp from "./pages/AddNewMeetUp";
+import Favorites from "./pages/Favorites";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllMeetups />} />
+          <Route path="/addNewMeetUp" element={<AddNewMeetUp />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
