3ad151fb83591b7e6ee539673d39d723268c6035
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-06 06:11:18 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import AddNewMeetUp from "./pages/AddNewMeetUp";
 import Favorites from "./pages/Favorites";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
-    <div>
+    <main className="w-full flex-center flex-col">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<AllMeetups />} />
           <Route path="/addNewMeetUp" element={<AddNewMeetUp />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </main>
   );
 };
 
