b7811780a5ee9305bbafd5b7e4b305c0a5bef136
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-08-08 02:40:22 +0100

    Fetch data from the database

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,12 +1,41 @@
-import React from "react";
-import { DUMMY_DATA } from "../components/meetUps/MeetUpData";
+import React, { useEffect, useState } from "react";
+// import { DUMMY_DATA } from "../components/meetUps/MeetUpData";
 import MeetUpLists from "../components/meetUps/MeetUpLists";
 
 const AllMeetups = () => {
+  const [loadedMeetUp, setLoadedMeetup] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://react-meetup-1f936-default-rtdb.firebaseio.com/meetup.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetUps = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetUps.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetup(meetUps);
+      });
+  }, []);
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetUpLists meetUpData={DUMMY_DATA} />
+
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <MeetUpLists meetUpData={loadedMeetUp} />
+      )}
     </section>
   );
 };
